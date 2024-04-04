5575530d44acc70f10d966ea29eab2d6d8752d3d
Dreamer <aleksandar.iack0vlev@mail.ru>
2024-04-04 17:51:49 +0300

    EditGekPage

--- src/pages/EditGekPage.js
+++ src/pages/EditGekPage.js
@@ -8,7 +8,8 @@ const EditGekPage = () => {
     const { gekId } = useParams(); // Получаем параметр маршрута
     const [membersGek, setMembersGek] = useState([]);
     const [allMembersGek, setAllMembersGek] = useState([]);
-    
+    const [selectedCategory, setSelectedCategory] = useState('Состав'); // Добавляем состояние для выбранной категории
+
     useEffect(() => {
         // Находим данные выбранной ГЭК по идентификатору из параметра маршрута
         const gek = geksData.find(gek => gek.id === parseInt(gekId));
@@ -24,8 +25,14 @@ const EditGekPage = () => {
     }, [gekId]);
     
     const handleMemberAdd = (member) => {
-        // Добавить участника в список текущих участников
-        setMembersGek([...membersGek, member]);
+        // Добавить участника в выбранную категорию
+        if (selectedCategory === 'Состав') {
+            setMembersGek([...membersGek, member]);
+        } else if (selectedCategory === 'Секретари') {
+            // Добавить участника в список секретарей
+            // Здесь предполагается, что у вас есть отдельный массив для секретарей в объекте gek или другой логике
+            // Например, gek.secretaries.push(member)
+        }
         
         // Удалить участника из списка доступных участников
         const updatedMembers = allMembersGek.filter(m => m.id !== member.id);
@@ -41,14 +48,27 @@ const EditGekPage = () => {
         setMembersGek(updatedMembers);
     };
 
+    const handleCategoryChange = (category) => {
+        setSelectedCategory(category);
+    };
+
+    // Сортировка по алфавиту ФИО
+    const sortedMembersGek = membersGek.slice().sort((a, b) => a.name.localeCompare(b.name));
+    const sortedAllMembersGek = allMembersGek.slice().sort((a, b) => a.name.localeCompare(b.name));
+
   return (
     <div className="container-fluid text-center my-3">
       <div className="row justify-content-evenly">
         <Card style={{ minWidth: '500px', width: '40%' }} className="my-2 text-center bg-light">
-            <Card.Body>
-                <Card.Title className="text-center fs-5">Текущие члены ГЭК</Card.Title>
-                <ListGroup className="container">
-                    {membersGek.map((member, index) => (
+            <Card.Header className="fs-4 bg-light">Текущие члены ГЭК №{gekId}</Card.Header>
+            <Card.Body> 
+                <Button 
+                    variant={selectedCategory === 'Состав' ? 'primary' : 'secondary'} 
+                    onClick={() => handleCategoryChange('Состав')}
+                    className="mx-3 my-2"
+                >Состав</Button>
+                <ListGroup className="container text-center">
+                    {selectedCategory === 'Состав' && sortedMembersGek.map((member, index) => (
                     <ListGroup.Item key={index} className="d-flex justify-content-between align-items-center py-2">
                         <span className="mx-3">{member.id}: {member.name}</span>
                         <Button 
@@ -57,17 +77,33 @@ const EditGekPage = () => {
                             className="mx-3"
                         >Удалить</Button>
                     </ListGroup.Item>
-                ))}
+                    ))}    
+                </ListGroup>
+                <Button 
+                    variant={selectedCategory === 'Секретари' ? 'primary' : 'secondary'} 
+                    onClick={() => handleCategoryChange('Секретари')}
+                    className="mx-3 my-2"
+                >Секретари</Button>
+                <ListGroup className="container text-center">
+                    {selectedCategory === 'Секретари' && sortedMembersGek.map((member, index) => (
+                    <ListGroup.Item key={index} className="d-flex justify-content-between align-items-center py-2">
+                        <span className="mx-3">{member.id}: {member.name}</span>
+                        <Button 
+                            variant="danger" 
+                            onClick={() => handleMemberRemove(member)}
+                            className="mx-3"
+                        >Удалить</Button>
+                    </ListGroup.Item>
+                    ))}    
                 </ListGroup>
             </Card.Body>
         </Card>
-
-
+    
         <Card style={{ minWidth: '500px', width: '40%' }} className="my-2 text-center bg-light">
             <Card.Body>
                 <Card.Title className="text-center fs-5">Члены ГЭК</Card.Title>
                 <ListGroup className="container">
-                    {allMembersGek.map((member, index) => (
+                    {sortedAllMembersGek.map((member, index) => (
                     <ListGroup.Item key={index} className="d-flex justify-content-between align-items-center py-2">
                         <span className="mx-3">{member.id}: {member.name}</span>
                         <Button 
