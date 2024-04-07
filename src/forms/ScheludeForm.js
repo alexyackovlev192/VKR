35c4ba103c717212278fb7ecc86c5c7294dfbd5c
Alexandar <aleksandar.iack0vlev@mail.ru>
2024-04-07 18:56:46 +0300

    add gorizontal scroll and fixed column "data"

--- src/forms/ScheludeForm.js
+++ src/forms/ScheludeForm.js
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const ScheludeForm = ({ formData, handleInputChange }) => {
   const { date, direction, time, gec, room } = formData || {};
-
-  const [selectedDate, setSelectedDate] = useState(null);
-
-  const handleDateChange = date => {
-    setSelectedDate(date);
-  };
-
+  const [selectedDate, setSelectedDate] = useState();
   return (
     <Form>
       <Form.Group controlId="formDate">
         <Form.Label>Дата</Form.Label>
         <div>
-            <DatePicker
-                selected={selectedDate}
-                onChange={handleDateChange}
-                dateFormat="dd.MM"
-                placeholderText={date}
-                className="form-control"
-                value={date}
-            />
+          <DatePicker
+            selected={selectedDate}
+            onChange={(newDate) => setSelectedDate(newDate)}
+            dateFormat="dd.MM"
+            placeholderText={date}
+            className="form-control"
+          />
         </div>
       </Form.Group>
       <Form.Group controlId="formDirection">
--- src/pages/SchedulePage.js
+++ src/pages/SchedulePage.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './style-pages/SchedulePage.css';
-import scheludesData from '../data/scheludesData.json';
+import schedulesData from '../data/schedulesData.json';
 import directionData from '../data/directionData.json';
-import UpdateSchelude from '../modal-windows/UpdateSchedule';
+import UpdateSchedule from '../modal-windows/UpdateSchedule';
 
 const SchedulePage = () => {
   const [editingData, setEditingData] = useState(null);
@@ -31,44 +31,62 @@ const SchedulePage = () => {
   };
 
   return (
-    <div className="schedule my-5 px-5">
-      <table className="table table-light table-hover text-center">
-        <thead>
-          <tr>
-            <th>Дата</th>
-            {directionData.map((item, index) => (
-              <th key={index}>{item.name}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {scheludesData.map((item, index) => (
-            <tr key={index}>
-              <td
-                className={activeCell === item ? 'table-active' : ''}
-                onClick={() => handleCellClick(item)}
-              >
-                {item.date}
-              </td>
-              {directionData.map((direction, idx) => (
-                <td
-                  key={idx}
-                  onDoubleClick={() =>
-                    openEditModal({
-                      date: item.date,
-                      direction: direction.id,
-                      event: item[direction.id]
-                    })
-                  }
-                >
-                  {item[direction.id]}
-                </td>
+    <div className="schedule-container my-5 px-5">
+      <div className="schedule-wrapper row">
+        <div className="col-1 px-0"> {/* Ширина первой колонки */}
+          <table className="table table-light table-hover text-center">
+            <thead>
+              <tr>
+                <th className="px-5">Дата</th>
+              </tr>
+            </thead>
+            <tbody>
+              {schedulesData.map((item, index) => (
+                <tr key={index}>
+                  <td
+                    className={activeCell === item ? 'table-active' : ''}
+                    onClick={() => handleCellClick(item)}
+                  >
+                    {item.date}
+                  </td>
+                </tr>
               ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <UpdateSchelude
+            </tbody>
+          </table>
+        </div>
+        <div className="col-11 px-0" style={{ overflowX: 'auto' }}> {/* Ширина второй колонки */}
+          <table className="table table-light table-hover text-center">
+            <thead>
+              <tr> 
+                {directionData.map((item, index) => (
+                  <th className="px-5" key={index}>{item.name}</th>
+                ))}
+              </tr>
+            </thead>
+              <tbody>
+                {schedulesData.map((item, index) => (
+                  <tr key={index}>
+                    {directionData.map((direction, idx) => (
+                      <td
+                        key={idx}
+                        onDoubleClick={() =>
+                          openEditModal({
+                            date: item.date,
+                            direction: direction.id,
+                            event: item[direction.id]
+                          })
+                        }
+                      >
+                        {item[direction.id]}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+          </table>
+        </div>  
+      </div>
+      <UpdateSchedule
         showModal={showModal}
         handleCloseModal={handleCloseModal}
         formData={editingData || {}}
