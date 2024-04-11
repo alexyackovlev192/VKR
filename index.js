6bb7af9e4d78583cccae1737ce767f5dad35a67a
Kirillkuk <khryavin@list.ru>
2024-04-11 13:48:59 +0300

    fix index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
-
+const connection = require('./db/connect');
+const authRouter = require('./routes/AuthRoutes')
+const adminRouter = require('./routes/AdminRoutes')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(express.json())
+app.use("/auth", authRouter)
+app.use("/admin", adminRouter)
 
-const start = () => {
+const start = () => { 
     try {
         app.listen(PORT, console.log(`server started on port ${PORT}`)) 
     }
@@ -14,3 +19,7 @@ const start = () => {
 }
 
 start()
+
+
+
+
