63a42847f52cc5cc4eee188d8ba497649dc60f2a
Kirillkuk <khryavin@list.ru>
2024-04-12 18:04:04 +0300

    fix index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const connection = require('./db/connect');
 const authRouter = require('./routes/AuthRoutes')
 const adminRouter = require('./routes/AdminRoutes')
+const gecMembersRouter = require('./routes/gecMembersRoutes')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
 app.use("/auth", authRouter)
 app.use("/admin", adminRouter)
-
+app.use("/gecMembers", gecMembersRouter)
 const start = () => { 
     try {
         app.listen(PORT, console.log(`server started on port ${PORT}`)) 
