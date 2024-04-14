c20a3d0a3f6b1ebbb40bbbde099b1f7b3aa2f2a6
Kirillkuk <khryavin@list.ru>
2024-04-14 19:28:54 +0300

    created routes for getting and updating secretaries Gec

---
+++ routes/secretariesGecRoutes.js
@@ -0,0 +1,13 @@
+const Router = require('express')
+const controller = require('../controllers/userController')
+const {check} = require("express-validator")
+const router = new Router()
+const authMiddleware = require('../middleware/authMiddleware')
+const roleMiddleware = require('../middleware/roleMiddleware')
+
+router.get('/', authMiddleware, roleMiddleware([3]), controller.getAllSecretariesGec)
+router.put('/:id', [
+    check('Fullname').optional().notEmpty().withMessage('ФИО пользователя не может быть пустым'),
+    check('Mail').optional().isEmail().withMessage('Почта пользователя не может быть пустой')
+], authMiddleware, roleMiddleware([3]), controller.updateSecretaryGec)
+module.exports = router
