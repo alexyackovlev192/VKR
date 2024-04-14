9cee5919360d0479d0f893206b42db182768069a
Kirillkuk <khryavin@list.ru>
2024-04-14 19:27:43 +0300

    created model Direction

---
+++ models/Direction.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/connect');
+
+const Direction = sequelize.define('Direction', {
+    id_D: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    Name_direction: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+        unique: true
+    },
+    code: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    }
+}, {
+    tableName: 'directions',
+    timestamps: false
+});
+
+module.exports = Direction;
