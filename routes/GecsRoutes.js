abbc63ec0207be9775c0f3b3bb16b09be13537b8
Kirillkuk <khryavin@list.ru>
2024-04-16 17:37:39 +0300

    created routes for gec and gecComposition

---
+++ routes/GecsRoutes.js
@@ -0,0 +1,15 @@
+const Router = require('express')
+const controller = require('../controllers/gecController')
+const {check} = require("express-validator")
+const router = new Router()
+const authMiddleware = require('../middleware/authMiddleware')
+const roleMiddleware = require('../middleware/roleMiddleware')
+
+router.post('/create', [
+    check('NameDirection',"Наименование направления не может быть пустым").notEmpty(),
+    check('FullnameSecretary', "ФИО секретаря гэк не может быть пустым").notEmpty(),
+    check('Year', "Год защит у комиссии не может быть пустым").notEmpty()
+], authMiddleware, roleMiddleware([3]), controller.create)
+router.get('/', authMiddleware, roleMiddleware([3]), controller.getAllGecs)
+
+module.exports = router
---
+++ routes/gecCompositionRoutes.js
@@ -0,0 +1,11 @@
+const Router = require('express')
+const controller = require('../controllers/compositionGecController')
+const {check} = require("express-validator")
+const router = new Router()
+const authMiddleware = require('../middleware/authMiddleware')
+const roleMiddleware = require('../middleware/roleMiddleware')
+
+router.put('/:id', authMiddleware, roleMiddleware([3]), controller.updateGecComposition)
+router.get('/:id', authMiddleware, roleMiddleware([3,2]), controller.getGecComposition)
+router.get('/UserGecs/:id', authMiddleware, roleMiddleware([2]), controller.getGecIdsByUserId);
+module.exports = router
