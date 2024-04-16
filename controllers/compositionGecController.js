db3b6208644774e93cd08edb32172ed1df0be459
Kirillkuk <khryavin@list.ru>
2024-04-16 17:38:32 +0300

    created controllers for gec and compositionGec

---
+++ controllers/compositionGecController.js
@@ -0,0 +1,80 @@
+const CompositionGec = require('../models/CompositionGec')
+const Gec = require('../models/Gec')
+
+class compositionGecController {
+    async updateGecComposition(req, res) {
+        const id = req.params.id;
+        const {memberIds, secretaryId} = req.body 
+        try {
+            if (!memberIds || !secretaryId) {
+                return res.status(400).json({ message: "Необходимо указать memberIds и secretaryId" });
+            }
+
+            const currentMembers = await CompositionGec.findAll({
+                where: { id_G: id }
+            });
+            
+            const currentMemberIds = currentMembers.map(member => member.id_U);
+    
+            const membersToDelete = currentMemberIds.filter(id => !memberIds.includes(id));
+    
+            const membersToAdd = memberIds.filter(id => !currentMemberIds.includes(id));
+    
+            if (membersToDelete.length > 0) {
+                await CompositionGec.destroy({
+                    where: {
+                        id_G: id,
+                        id_U: membersToDelete
+                    }
+                });
+            }
+            
+            if (membersToAdd.length > 0) {
+                const newMembers = membersToAdd.map(id_U => ({
+                    id_G: id,
+                    id_U: id_U
+                }));
+                await CompositionGec.bulkCreate(newMembers);
+            }
+                                   
+            await Gec.update({ id_U: secretaryId },
+                { where: { id_G: id } }
+            );
+            
+            res.status(200).json({ message: 'Состав ГЭК успешно обновлен' });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Ошибка при обновлении состава ГЭК', error });
+        }
+    }
+    async getGecComposition(req, res) {
+        try {
+            const id = req.params.id;
+            if (!id) {
+                res.status(400).json({message: "Id не указан"})
+            }
+            const compositionGec  = await CompositionGec.findAll({where: {id_G: id}})
+            return res.json(compositionGec)
+        }
+        catch(e) {
+            return res.status(500).json(e)
+        }
+    }
+    async getGecIdsByUserId(req, res) {
+        try {
+            const id = req.params.id;
+            if (!id) {
+                res.status(400).json({message: "Id не указан"})
+            }
+            const gecIds = await CompositionGec.findAll({
+                where: { id_U: id },
+                attributes: ['id_G'] 
+            });
+            res.status(200).json(gecIds);
+        }
+        catch (e) {
+            res.status(500).json({ message: 'Ошибка при получении id_G по id_U', e });
+        }
+    }
+}
+module.exports = new compositionGecController()
---
+++ controllers/gecController.js
@@ -0,0 +1,42 @@
+const Gec = require('../models/Gec')
+const Direction = require('../models/Direction')
+const User = require('../models/User')
+const {validationResult} = require('express-validator')
+
+class gecController {
+    async create(req, res) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message:"Ошибка при создании ГЭК", errors})
+            }
+            const {NameDirection, FullnameSecretary, Year} = req.body
+            const direction = await Direction.findOne({where: {
+                Name_direction: NameDirection
+            }})
+            
+             const secretaryGec = await User.findOne({where: {
+                Fullname: FullnameSecretary
+             }})
+
+             const gec = new Gec({ id_D: direction.id_D, id_U: secretaryGec.id_U, year:  Year });
+             await gec.save()
+             return res.json({message: "ГЭК успешно создана"})
+        } catch(e) {
+            console.log(e)
+            res.status(400).json({message: 'Ошибка создания ГЭК'})
+        }
+    }
+    async getAllGecs(req, res) {
+        try {
+            const gecs = await Gec.findAll({where: {status: null}})
+            if (!gecs || gecs.length === 0) {
+                return res.status(404).json({ message: 'ГЭКи не найдены' });
+            }
+            return res.json(gecs)
+        } catch(e) {
+            return res.status(500).json(e)
+        }
+    }
+}
+module.exports = new gecController()
