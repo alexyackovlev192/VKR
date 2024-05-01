79c5b9c36117f6b3704a76a64f003bbbdf2747d7
Kirillkuk <khryavin@list.ru>
2024-05-01 17:04:30 +0300

    created model for Student

---
+++ models/Student.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/connect');
+
+const Student = sequelize.define('Student', {
+    id_S: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    Fullname: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    Group: {
+        type: DataTypes.STRING(10),
+        allowNull: false 
+    },
+    Topic: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    },
+    ScientificAdviser: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    Avg_Mark: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    Red_Diplom: {
+        type: DataTypes.STRING(30),
+        allowNull: true
+    },
+    YearOfDefense: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    },
+    Name_direction: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    }
+}, {
+    tableName: 'students',
+    timestamps: false
+});
+
+module.exports = Student;
