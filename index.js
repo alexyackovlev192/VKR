89fc1bedc00a118c3f4c97a0a6fae91dbe3df105
Kirillkuk <khryavin@list.ru>
2024-05-01 23:01:50 +0300

    added routes for defenseSchedule

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const secretariesGecRouter = require('./routes/secretariesGecRoutes')
 const GecsRouter = require('./routes/GecsRoutes')
 const gecCompositionRouter = require('./routes/gecCompositionRoutes')
 const StudentsRouter = require('./routes/StudentsRoutes')
+const DefenseScheduleRouter = require('./routes/DefenseScheduleRoutes')
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -18,6 +19,7 @@ app.use("/secretariesGec", secretariesGecRouter)
 app.use("/gecs", GecsRouter)
 app.use("/gecComposition", gecCompositionRouter)
 app.use("/students", StudentsRouter)
+app.use("/defenseSchedule", DefenseScheduleRouter)
 const start = () => { 
     try {
         app.listen(PORT, console.log(`server started on port ${PORT}`)) 
