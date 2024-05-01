2e0df47ac7467d649334349be47dc4785e22a70c
Kirillkuk <khryavin@list.ru>
2024-05-01 18:04:45 +0300

    fix Student Controller

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -13,7 +13,7 @@ class studentController {
             const direction = await Direction.findOne({where: {
                 Name_direction: NameDirection
             }})
-            
+
             if (!direction) {
                 return res.status(404).json({ message: "Направление не найдено" });
             }
@@ -26,5 +26,42 @@ class studentController {
             res.status(400).json({message: 'Ошибка добавления студента'})
         }
     }
+    async updateStudent(req, res) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message:"Ошибка при обновлении информации о студенте", errors})
+            }
+            const { id } = req.params;
+            const {Fullname, Group, Topic, ScientificAdviser, Avg_Mark, Red_Diplom, YearOfDefense, NameDirection} = req.body 
+            if (!id) {
+                res.status(400).json({message: "Id не указан"})
+            }
+            if (NameDirection) {
+                const direction = await Direction.findOne({
+                    where: {
+                        Name_direction: NameDirection
+                    }
+                });
+    
+                if (!direction) {
+                    return res.status(404).json({ message: "Направление не найдено" });
+                }
+            }
+            const updatedRowsCount = await Student.update(
+                { Fullname, Group, Topic, ScientificAdviser, Avg_Mark, Red_Diplom, YearOfDefense, Name_direction: NameDirection },
+                { where: { id_S: id } }
+            )
+            if (updatedRowsCount[0] === 0) {
+                return res.status(404).json({ message: `Нет студента с таким идентификатором: ${id}` })
+            }
+            const updatedStudent = await Student.findOne({ where: { id_S: id } }); 
+            return res.status(200).json(updatedStudent);
+
+        } catch(e) {
+            res.status(500).json({ message: 'Ошибка при обновлении данных студента', e})
+        }
+    }
+
 }
 module.exports = new studentController()
