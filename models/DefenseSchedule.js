9b10875462a4e32e48b2190bf323f86c290163bf
Kirillkuk <khryavin@list.ru>
2024-05-01 22:59:29 +0300

    created DefenseSchedule model

---
+++ models/DefenseSchedule.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/connect');
+
+const DefenseSchedule = sequelize.define('DefenseSchedule', {
+    id_DS: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    id_G: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    Name_direction: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    time: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    classroom: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    }
+}, {
+    tableName: 'defense_schedule',
+    timestamps: false
+});
+
+module.exports = DefenseSchedule;
