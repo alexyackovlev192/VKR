79b3f80360dae65b00a4c551340637e48d77efba
Kirillkuk <khryavin@list.ru>
2024-05-01 23:00:51 +0300

    added functionality for creating and updating defense

---
+++ controllers/defenseScheduleController.js
@@ -0,0 +1,80 @@
+const DefenseSchedule = require('../models/DefenseSchedule')
+const Direction = require('../models/Direction')
+const Gec = require('../models/Gec')
+const {validationResult} = require('express-validator')
+
+class defenseScheduleController {
+    async create(req, res) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message:"Ошибка при создании защиты", errors})
+            }
+            const {GecId, NameDirection, Date, Time, Classroom} = req.body
+            const direction = await Direction.findOne({where: {
+                Name_direction: NameDirection
+            }})
+            if (!direction) {
+                return res.status(404).json({ message: "Направление не найдено" });
+            }
+            const gec = await Gec.findOne({where: {
+                id_G: GecId
+            }})
+            if (!gec) {
+                return res.status(404).json({ message: "ГЭК не найдена" });
+            }
+
+            const defenseSchedule = new DefenseSchedule({ id_G: gec.id_G, Name_direction: direction.Name_direction, date:  Date, time: Time, classroom: Classroom });
+            await defenseSchedule.save()
+            return res.json({message: "Защита успешно создана"})
+        } catch(e) {
+            console.log(e)
+            res.status(400).json({message: 'Ошибка создания защиты'})
+        }
+    }
+    async updateDefense(req, res) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message:"Ошибка при обновлении информации о защите", errors})
+            }
+            const { id } = req.params;
+            const {GecId, NameDirection, Date, Time, Classroom} = req.body 
+            if (!id) {
+                res.status(400).json({message: "Id не указан"})
+            }
+            if (NameDirection) {
+                const direction = await Direction.findOne({
+                    where: {
+                        Name_direction: NameDirection
+                    }
+                });
+    
+                if (!direction) {
+                    return res.status(404).json({ message: "Направление не найдено" });
+                }
+            }
+            if (GecId) {
+                const gec = await Gec.findOne({where: {
+                    id_G: GecId
+                }})
+                if (!gec) {
+                    return res.status(404).json({ message: "ГЭК не найдена" });
+                }
+            }
+            const updatedRowsCount = await DefenseSchedule.update(
+                { id_G: GecId, Name_direction: NameDirection, date: Date, time: Time, classroom: Classroom },
+                { where: { id_DS: id } }
+            )
+            if (updatedRowsCount[0] === 0) {
+                return res.status(404).json({ message: `Нет защиты с таким идентификатором: ${id}` })
+            }
+            const updatedDefense = await DefenseSchedule.findOne({ where: { id_DS: id } }); 
+            return res.status(200).json(updatedDefense);
+
+        } catch(e) {
+            res.status(500).json({ message: 'Ошибка при обновлении данных защиты', e})
+        }
+    }
+}
+module.exports = new defenseScheduleController()
---
+++ routes/DefenseScheduleRoutes.js
@@ -0,0 +1,23 @@
+const Router = require('express')
+const controller = require('../controllers/defenseScheduleController')
+const {check} = require("express-validator")
+const router = new Router()
+const authMiddleware = require('../middleware/authMiddleware')
+const roleMiddleware = require('../middleware/roleMiddleware')
+
+router.post('/create', [
+    check('GecId',"Id ГЭК не может быть пустым").notEmpty(),
+    check('NameDirection', "Название направления не может быть пустым").notEmpty(),
+    check('Date', "Дата защиты не может быть пустой").notEmpty(),
+    check('Time', "Время защиты не может быть пустым").notEmpty(),
+    check('Classroom', "Аудитория защиты не может быть пустой").notEmpty(),
+], authMiddleware, roleMiddleware([3]), controller.create)
+router.put('/:id', [
+    check('GecId').optional().notEmpty().withMessage('Id ГЭК не может быть пустым'),
+    check('NameDirection').optional().notEmpty().withMessage('Название направления не может быть пустым'),
+    check('Date').optional().notEmpty().withMessage('Дата защиты не может быть пустой'),
+    check('Time').optional().notEmpty().withMessage('Время защиты не может быть пустым'),
+    check('Classroom').optional().notEmpty().withMessage('Аудитория защиты не может быть пустой')
+], authMiddleware, roleMiddleware([3]), controller.updateDefense)
+
+module.exports = router
