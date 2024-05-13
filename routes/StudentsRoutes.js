14e889e4a473b3211b5fd76cc98f8b29c7e7b6c4
Alex <alek2002sandr@mail.ru>
2024-05-13 21:15:22 +0300

    x

--- routes/StudentsRoutes.js
+++ routes/StudentsRoutes.js
@@ -1,19 +1,20 @@
-const Router = require('express')
-const controller = require('../controllers/studentController')
-const {check} = require("express-validator")
-const router = new Router()
-const authMiddleware = require('../middleware/authMiddleware')
-const roleMiddleware = require('../middleware/roleMiddleware')
+const Router = require('express');
+const controller = require('../controllers/studentController');
+const { check } = require("express-validator");
+const router = new Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
 
 router.post('/create', [
-    check('Fullname',"ФИО студента не может быть пустым").notEmpty(),
+    check('Fullname', "ФИО студента не может быть пустым").notEmpty(),
     check('Group', "Группа студента не может быть пустой").notEmpty(),
     check('Topic', "Тема ВКР не может быть пустой").notEmpty(),
     check('ScientificAdviser', "ФИО научного руководителя не может быть пустым").notEmpty(),
     check('Avg_Mark', "Средний балл студента не может быть пустым").notEmpty(),
     check('YearOfDefense', "Год защиты студента не может быть пустым").notEmpty(),
-    check('NameDirection', "Направление студента не может быть пустым").notEmpty()
-], authMiddleware, roleMiddleware([3]), controller.create)
+    check('Name_direction', "Направление студента не может быть пустым").notEmpty()
+], authMiddleware, roleMiddleware([1,3]), controller.create);
+
 router.put('/:id', [
     check('Fullname').optional().notEmpty().withMessage('ФИО студента не может быть пустым'),
     check('Group').optional().notEmpty().withMessage('Группа студента не может быть пустой'),
@@ -21,9 +22,9 @@ router.put('/:id', [
     check('ScientificAdviser').optional().notEmpty().withMessage('ФИО научного руководителя не может быть пустым'),
     check('Avg_Mark').optional().notEmpty().withMessage('Средний балл студента не может быть пустым'),
     check('YearOfDefense').optional().notEmpty().withMessage('Год защиты студента не может быть пустым'),
-    check('NameDirection').optional().notEmpty().withMessage('Направление студента не может быть пустым'),
-], authMiddleware, roleMiddleware([3]), controller.updateStudent)
-router.get('/thisYear', authMiddleware, roleMiddleware([3]), controller.getStudentsDefThisYear)
+    check('Name_direction').optional().notEmpty().withMessage('Направление студента не может быть пустым'),
+], authMiddleware, roleMiddleware([3]), controller.updateStudent);
+router.get('/thisYear', authMiddleware, roleMiddleware([1,3]), controller.getStudentsDefThisYear)
 router.get('/:id', authMiddleware, roleMiddleware([3,2,4]), controller.getStudentById)
 
 module.exports = router
