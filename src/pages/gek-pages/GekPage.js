23ba6e5f27f0bc8eb661fdb833e8fd8540456b48
Alex <alek2002sandr@mail.ru>
2024-05-15 17:54:57 +0300

    create method for view geks

--- src/pages/gek-pages/GekPage.js
+++ src/pages/gek-pages/GekPage.js
@@ -4,72 +4,115 @@ import axios from 'axios';
 import { Button, Card, ListGroup } from 'react-bootstrap';
 
 const GekPage = () => {
-  const [geks, setGeks] = useState([]); // Хранение данных ГЭК в состоянии
-  const [members, setMemberss] = useState([]);
+  const [geks, setGeks] = useState([]);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
+
     axios.get('http://localhost:5000/gecs', {
       headers: {
         'Authorization': `Bearer ${token}`
       }
     })
     .then(response => {
-      setGeks(response.data);
+      const fetchedGeks = response.data;
+      setGeks(fetchedGeks);
+
+      fetchedGeks.forEach(gek => {
+        axios.get(`http://localhost:5000/gecComposition/${gek.id_G}`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        })
+        .then(compResponse => {
+          const compositions = compResponse.data;
+
+          const compositionPromises = compositions.map(comp => 
+            axios.get(`http://localhost:5000/users/${comp.id_U}`, {
+              headers: {
+                'Authorization': `Bearer ${token}`
+              }
+            }).then(memberResponse => ({
+              ...comp,
+              members: memberResponse.data
+            }))
+          );
+
+          Promise.all(compositionPromises)
+          .then(compositionsWithMembers => {
+            axios.get(`http://localhost:5000/directions/${gek.id_D}`, {
+              headers: {
+                'Authorization': `Bearer ${token}`
+              }
+            })
+            .then(directsResponse => {
+              const directs = directsResponse.data;
+
+              setGeks(prevGeks => {
+                return prevGeks.map(g => {
+                  if (g.id === gek.id) {
+                    return { ...g, compositions: compositionsWithMembers, directs };
+                  }
+                  return g;
+                });
+              });
+            })
+            .catch(error => console.error('Ошибка при загрузке директорий:', error));
+          });
+        })
+        .catch(error => console.error('Ошибка при загрузке составов:', error));
+      });
     })
-    .catch(error => console.error('Ошибка при загрузке данных:', error));
-    axios.get('http://localhost:5000/gecComposition/3', {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
-    .then(response => {
-      setMemberss(response.data);
-    })
-    .catch(error => console.error('Ошибка при загрузке данных:', error));
+    .catch(error => console.error('Ошибка при загрузке ГЭК:', error));
   }, []);
 
-  // // Функция для удаления ГЭК по идентификатору
-  // const handleDeleteGek = (id) => {
-  //   // Фильтрация массива ГЭК, оставляя только те, у которых id не совпадает с удаляемым id
-  //   const updatedGeks = geks.filter(gek => gek.id !== id);
-  //   setGeks(updatedGeks); // Обновление состояния
-  // };
+  console.log(geks);
 
-  return (
-    <div className="container-fluid text-center my-3">
-      <Link to={`/create-gek`}>
-        <Button variant="primary" className="col-2 py-2">Создать новую ГЭК</Button>
-      </Link>
 
-      {/* <div className="row justify-content-evenly">
-        {geks.map(gekData => (
-          <Card key={gekData.id} style={{ minWidth: '400px', width: '30%' }}  className="col-4 my-4 text-center bg-light">
-            <Card.Header className="fs-4 bg-light" >ГЭК №{gekData.number}</Card.Header>
-            <Card.Body> 
-              <Card.Title className="text-center fs-5">Состав</Card.Title>
-              <ListGroup className="text-center">
-                {gekData.members.map((member, index) => (
-                  <ListGroup.Item key={index}>{member.name} - {member.position}</ListGroup.Item>
-                ))}
-              </ListGroup>
-              <Card.Title className="text-center fs-5">Секретари</Card.Title>
-              <ListGroup className="text-center">
-                {gekData.secretaries.map((secretary, index) => (
-                  <ListGroup.Item key={index}>{secretary.name}</ListGroup.Item>
-                ))}
-              </ListGroup>
-            </Card.Body>
-            <Card.Footer className="text-left bg-light">
-              <Link to={`/edit-gek/${gekData.id}`}>
-                <Button variant="primary" className="mx-3">Редактировать</Button>
-              </Link>
-              <Button variant="danger" className="mx-3" onClick={() => handleDeleteGek(gekData.id)}>Удалить</Button>
-            </Card.Footer>
-          </Card>
-        ))}
-      </div> */}
-    </div>
+ return (
+    // <div className="container-fluid text-center my-3">
+    //   <Link to={`/create-gek`}>
+    //     <Button variant="primary" className="col-2 py-2">Создать новую ГЭК</Button>
+    //   </Link>
+    //   <div className="row justify-content-evenly">
+    //     {geks && geks.map(gekData => (
+    //       <Card key={gekData.id} style={{ minWidth: '400px', width: '30%' }} className="col-4 my-4 text-center bg-light">
+    //         <Card.Header className="fs-4 bg-light">ГЭК №{gekData.number}</Card.Header>
+    //         <Card.Body>
+    //           <Card.Title className="text-center fs-5">Состав</Card.Title>
+    //           <ListGroup className="text-center">
+    //             {gekData.compositions && gekData.compositions.map((comp, compIndex) => (
+    //               <div key={compIndex}>
+    //                 {comp.members && comp.members.map((member, memberIndex) => (
+    //                   <ListGroup.Item key={memberIndex}>{member.Fullname} - {member.Post}</ListGroup.Item>
+    //                 ))}
+    //               </div>
+    //             ))}
+    //           </ListGroup>
+    //           <Card.Title className="text-center fs-5">Секретарь</Card.Title>
+    //           <ListGroup className="text-center">
+    //             {gekData.secretaries && gekData.secretaries.map((secretary, index) => (
+    //               <ListGroup.Item key={index}>{secretary.name}</ListGroup.Item>
+    //             ))}
+    //           </ListGroup>
+    //           <Card.Title className="text-center fs-5">Направления</Card.Title>
+    //           <ListGroup className="text-center">
+    //             {gekData.directs && gekData.directs.map((direct, index) => (
+    //               <ListGroup.Item key={index}>{direct.name}</ListGroup.Item>
+    //             ))}
+    //           </ListGroup>
+    //         </Card.Body>
+    //         <Card.Footer className="text-left bg-light">
+    //           <Link to={`/edit-gek/${gekData.id}`}>
+    //             <Button variant="primary" className="mx-3">Редактировать</Button>
+    //           </Link>
+    //           {/* <Button variant="danger" className="mx-3" onClick={() => handleDeleteGek(gekData.id)}>Удалить</Button> */}
+    //         </Card.Footer>
+    //       </Card>
+    //     ))}
+    //   </div>
+    // </div>
+    <div></div>
   );
 };
 
