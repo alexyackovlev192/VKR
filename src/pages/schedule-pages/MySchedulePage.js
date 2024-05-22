bf7ac37e7ad62d82af456045187b2b1756cc2770
Alex <alek2002sandr@mail.ru>
2024-05-22 09:30:45 +0300

    fix data for MySchedules

--- src/pages/schedule-pages/MySchedulePage.js
+++ src/pages/schedule-pages/MySchedulePage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Card } from 'react-bootstrap';
 import axios from 'axios';
-import { jwtDecode } from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 
 import '../style-pages/MySchedulePage.css';
 
@@ -21,23 +21,33 @@ const MySchedulePage = () => {
                         'Authorization': `Bearer ${token}`
                     }
                 });
+
                 const schedulesWithDetails = await Promise.all(gekResponse.data.map(async (gek) => {
                     const detailedGekResponse = await axios.get(`http://localhost:5000/defenseSchedule/${gek.id_G}`, {
                         headers: {
                             'Authorization': `Bearer ${token}`
                         }
                     });
-                    const detailedGek = detailedGekResponse.data;
-                
 
-                    // const directsResponse = await axios.get(`http://localhost:5000/directions/${detailedGek.id_D}`, {
-                    //     headers: {
-                    //         'Authorization': `Bearer ${token}`
-                    //     }
-                    // });
+                    const defenseSchedules = detailedGekResponse.data;
 
-                    return { ...detailedGek };
+                    // Fetch directions for each defense schedule
+                    const schedulesWithDirections = await Promise.all(defenseSchedules.map(async (schedule) => {
+                        const directionResponse = await axios.get(`http://localhost:5000/directions/${schedule.id_D}`, {
+                            headers: {
+                                'Authorization': `Bearer ${token}`
+                            }
+                        });
+
+                        return {
+                            ...schedule,
+                            Name_direction: directionResponse.data.Name_direction
+                        };
+                    }));
+
+                    return schedulesWithDirections;
                 }));
+
                 console.log(schedulesWithDetails);
                 setScheduleDetails(schedulesWithDetails);
             } catch (error) {
@@ -48,11 +58,14 @@ const MySchedulePage = () => {
         fetchSchedules();
     }, []);
 
+    // Flattening the array of arrays for rendering purposes
+    const flattenedScheduleDetails = scheduleDetails.flat();
+
     return (
         <div className="container-fluid text-center my-3">
             <h4 className="col-12">Мои защиты</h4>
             <div className="row justify-content-evenly col-12 mx-3">
-                {scheduleDetails.map((item, index) => (
+                {flattenedScheduleDetails.map((item, index) => (
                     <Card key={index} className="col-2 mx-2 my-4 text-center bg-light">
                         <Card.Body>
                             <Card.Title>{new Date(item.date).toLocaleDateString('ru', { day: '2-digit', month: '2-digit' })}</Card.Title>
