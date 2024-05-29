27be85a60dd8f560e9ef7c667e2fe0d355a5cbf8
Alex <alek2002sandr@mail.ru>
2024-05-29 10:30:47 +0300

    xxx

--- src/pages/defenders-pages/DefendersPage.js
+++ src/pages/defenders-pages/DefendersPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useRef, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
